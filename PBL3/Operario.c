a046192a197aeb1788ce36ddaafa0829b98de5ef
Antonio Raian <raymendesjr2013@gmail.com>
2019-01-31 22:07:26 -0300

    Adição da descrição e de funcionalidades do sistema operario

--- PBL3/Operario.c
+++ PBL3/Operario.c
@@ -1,77 +1,141 @@
 #pragma config(Sensor, S1,     LightSensorLeft, sensorLightActive)
 #pragma config(Sensor, S2,     LightSensorRight, sensorLightActive)
 #pragma config(Sensor, S3,     sonar,          sensorSONAR)
-#pragma config(Motor,  motorA,          LeftMotor,     tmotorNormal, PIDControl, encoder)
-#pragma config(Motor,  motorB,          RightMotor,    tmotorNormal, PIDControl, encoder)
+#pragma config(Motor, motorA, motorEsq, tmotorNormal, PIDControl, encoder)
+#pragma config(Motor, motorB,  motorDir, tmotorNormal, PIDControl, encoder)
 #pragma platform(NXT)
 /////////////////////////////////////////////////////////////////////////////////////////
 
-char comando = 'n';
+//Estados
+const int PARADO = 0;
+const int EM_FRENTE_22 = 1;
+const int EM_FRENTE_27 = 2;
+const int VIRA_DIREITA = 3;
+const int VIRA_ESQUERDA = 4;
 
-const int maxTamMessage = 5;
-const TMailboxIDs entrada1 = mailbox1;
+//PROTOCOLO
+const int FRENTE_22 = 10;
+const int FRENTE_27 = 11;
+const int ESQUERDA = 2;
+const int DIREITA = 3;
 
-//Bluetooth ---------------------------------
-/*task checaConec()
-{
-   if (nBTCurrentStreamIndex >= 0){
-     nxtDisplayCenteredTextLine(1, "Conectado!!");
-     return;
-   }
+//Variaveis
+int estado = PARADO;
+int tickRobot = 20;
+int circunferenciaRobot = 628;
 
-   PlaySound(soundLowBuzz);
-   eraseDisplay();
-   nxtDisplayCenteredTextLine(3, "BT nao");
-   nxtDisplayCenteredTextLine(4, "Conectado");
-   wait1Msec(2000);
-   //StopAllTasks();
+////////////BlueTooth////////////////////////////////////////////
+void readBTMsg() {
+  ubyte bufferEntrada[1];                                       // create a ubyte array, 'BytesRead' of size 'bufferSize'.
+  int nNumbBytesRead;
+  nNumbBytesRead = nxtReadRawBluetooth(&bufferEntrada[0], 1);   // store 'bufferSize' amount of bytes into 'BytesRead[0]'.
+  nxtDisplayCenteredTextLine(5, "MSG %c", bufferEntrada[0]);
+
+  switch (bufferEntrada[0]){
+    case FRENTE_22:
+        estado = EM_FRENTE_22;
+        break;
+    case FRENTE_27:
+        estado = EM_FRENTE_27;
+        break;
+    case ESQUERDA:
+        estado = VIRA_ESQUERDA;
+        break;
+    case DIREITA:
+        estado = VIRA_DIREITA;
+        break;
+    default:
+        //estado = PARADO;
+        break;
+  }
 }
 
-task readDataMsg(){
-  int messageTam;
-  char bufferEntrada[maxTamMessage];
-
-      messageTam = cCmdMessageGetSize(entrada1);
-      if(messageTam<=0)
-        return;
-
-     cCmdMessageRead(bufferEntrada, messageTam, entrada1);
-     nxtDisplayCenteredTextLine(5, "MSG %c", bufferEntrada[0]);
-
-     if(bufferEntrada[0] == 'C' || bufferEntrada[0] == 'c')
-       //iniciar = true;
-     else if(bufferEntrada[0] == 'P' || bufferEntrada[0] == 'p')
-       //iniciar = false;
-   return;
-}*/
-
-task readDataMsg(){
-  setBluetoothOn();
+task configBT(){
+  //setBluetoothOn();
   setBluetoothRawDataMode();
-  while(!bBTRawMode){ //Espera entrar no modo
-        wait1Msec(50);
+  while (!bBTRawMode) {
+    nxtDisplayCenteredTextLine(4, "Inside RAW");
+		wait1Msec(1);
   }
-  while(true){
-    nxtReadRawBluetooth(&comando, 20);
+	nxtDisplayCenteredTextLine(4, "After RAW");
+  while(true) {
+    readBTMsg();
+    wait1Msec(100);
   }
-  EndTimeSlice();
+}
+void sendBTMessage(){
+  setBluetoothRawDataMode();
+  ubyte myMsg[1];
+	myMsg[0] = 0;
+	nxtWriteRawBluetooth(nBTCurrentStreamIndex , myMsg, 1);
+}
+//////////////////////////////////////////////////////////////////////////
+////////////////Funcoes///////////////////////////////////////////////////
+void parar(){
+	motor[motorDir] = 0;
+	motor[motorEsq] = 0;
+	wait1Msec(50);
 }
 
-void sendMessage(){
-  ubyte bytesEnviados = 10;
-
-  nxtWriteRawBluetooth(bytesEnviados,8);
-  wait1Msec(100);
+void viraGraus(int graus, int direcao, int forcaMotor){
+	int distanciaGraus = (graus * (circunferenciaRobot/10))/ 360; //calculo para verificar a distancia para percorrer em torno do seu proprio eixo.
+	distanciaGraus*=10;                                             //transformo para milimetro novamente.
+	nxtDisplayCenteredTextLine(2,"%d", distanciaGraus);
+	nMotorEncoder[motorEsq]  = 0;
+	nMotorEncoder[motorDir] = 0;
+	int tickGoal = ((tickRobot-2) * distanciaGraus)/10;
+	if(direcao == 1){ //turn left
+		while(nMotorEncoder[motorB] < tickGoal && nMotorEncoder[motorA] > (tickGoal*(-1))){
+			motor[motorEsq] = (-1)*(forcaMotor);//
+			motor[motorDir] = forcaMotor;
+		}
+	}else{ //turn Right
+		while(nMotorEncoder[motorB] > (tickGoal*(-1)) && nMotorEncoder[motorA] < tickGoal){
+			motor[motorEsq] = forcaMotor;
+			motor[motorDir] = (-1)*(forcaMotor);//
+		}
+	}
+	parar();
 }
-//////////////////////////////////////////////////////////////////////////////////////
-
+void walk(int distance, int powerMotor){
+    nMotorEncoder[motorEsq]  = 0;
+    nMotorEncoder[motorDir] = 0;
+    int tickGoal = (tickRobot * distance)/10;
+    //nMotorEncoder[motorA] = tickGoal;
+    //nMotorEncoder[motorB] = tickGoal;
+    while(nMotorEncoder[motorB] < tickGoal && nMotorEncoder[motorA] < tickGoal){
+      motor[motorEsq] = powerMotor;
+      motor[motorDir] = powerMotor;
+    }
+    parar();
+}
+//////////////////////////////////////////////////
 //PRINCIPAL-----------------------------------------------------------------------------
 task main{
-  StartTask(readDataMsg);
+  StartTask(configBT);
 
   while(true){
-	  if(nNxtButtonPressed == 3){//button orange pressed
-	    sendMessage();
+	  switch(estado){
+	    case PARADO:
+	      sendBTMessage();
+	      parar();
+        break;
+      case EM_FRENTE_22:
+        walk(22, 50);
+        estado = PARADO;
+        break;
+      case EM_FRENTE_27:
+        walk(27, 50);
+        estado = PARADO;
+        break;
+      case VIRA_DIREITA:
+        viraGraus(90, 0, 50);
+        estado = PARADO;
+        break;
+      case VIRA_ESQUERDA:
+        viraGraus(90, 1, 50);
+        estado = PARADO;
+        break;
 	  }
 	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NXTSeguidorLinha
+﻿# NXTSeguidorLinha
 LegoNXT seguidor de linha programado com RobotC, realizando comunicação via Bluethoot usando NXTComm Processing.
 
 http://www.robotc.net/wikiarchive/NXT
@@ -8,3 +8,22 @@ http://help.robotc.net/WebHelpMindstorms/index.htm
 http://jorgecardoso.eu/processing/NXTComm/
 
 https://processing.org/
+
+
+PBL3
+
+Esse projeto permite colocar um robô em um sistema virtual e gerar rotas entre dois pontos.
+O método usado para calcular rotas foi o Geometria de Manhattan.
+
+Protocolo  de comunicação:
+	- 10: Robô seguirá em frente por 22cm;
+	- 11: Robô seguirá em frente por 27cm;
+	- 2: Robô gira 90º para a direita sobre o próprio eixo;
+	- 3: Robô gira 90º para a esquerda sobre o próprio eixo;
+
+Estados:
+	- PARADO: Estado para o robô e envia mensagem para o supervisor informando que terminou a execução solicitada;
+	- EM_FRENTE_22: Estado fará com que o robô ande por 22cm em frente;
+	- EM_FRENTE_27: Estado fará com que o robô ande por 27cm em frente; 
+	- VIRA_DIREITA: Estado fará o robô girar 90º para a direita no próprio eixo;
+	- VIRA_ESQUERDA: Estado fará o robô girar 90º para a esquerda no próprio eixo;
